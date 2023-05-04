7e7c883383f8a180a24e9673d0360d469aab55f5
ariBruce <Aribruce1998@gmail.com>
2023-05-04 14:15:56 +0300

    these 3p and inits work work

---
+++ count3p_prototype.py
@@ -0,0 +1,20 @@
+import sys
+
+genome_hash = {}
+
+# actual genome file
+with open(sys.argv[1], 'r') as file1:
+    for line in file1:
+        chrom, length = line.strip().split('\t')
+        genome_hash[chrom] = [0] * (int(length) + 1)
+file1.close()
+with open(sys.argv[2], 'r') as file2:
+    for line in file2:
+        chrom, start, end, location, quality, strand = line.strip().split('\t')
+        genome_hash[chrom][int(end)] += 1
+file2.close()
+for keys, array in genome_hash.items():
+    count = 0
+    for index, value in enumerate(array):
+        print(keys, count, value, sep='\t')
+        count += 1
