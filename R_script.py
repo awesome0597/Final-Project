f9483a28b389946614795b08bc365c9812f0b2a6
awesome0597 <adira.weiss@gmail.com>
2023-05-17 17:07:46 +0300

    Added GUI and took care of global variables

--- R_script.py
+++ R_script.py
@@ -2,9 +2,9 @@ import statistics
 import sys
 import numpy as np
 import pandas as pd
-
-
 # from Bio import SeqIO
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QFormLayout, QGroupBox, QLineEdit, QDialogButtonBox, \
+    QComboBox
 
 
 def createFile(gene_list, score_a, score_b, score_c, my_init, my_3p, my_cov, number_list_appended):
@@ -26,7 +26,7 @@ def stats(my_cov, start, end):
     return mean, std
 
 
-def calculateScores(my_number_list, my_cov, my_length):
+def calculateScores(my_number_list, my_cov, my_length, win_size, W):
     # we can consider using a numpy array instead of a list
     score_a = [0] * my_length
     score_b = [0] * my_length
@@ -86,15 +86,15 @@ def covAndLen(init_library, three_p_library):
     return my_new_init, my_new_3p, cov, len(my_new_init)
 
 
-if __name__ == '__main__':
+def runScript(sequencing_type, window_size, genome_file_path, init_file_path, three_p_file_path, fasta_file_path,
+              output_file_name):
     # The program takes a genome file, init file, 3p file, fasta file path and known snoRNA info as arguments
-    win_size = 6
-    W = (1 + (1 - 0.1 * win_size)) * win_size / 2
+    W = (1 + (1 - 0.1 * window_size)) * window_size / 2
 
     # process genomes to work by size
     gene_list_per_base_pair = []
     number_list = []
-    with open(sys.argv[1], 'r') as file1:
+    with open(genome_file_path, 'r') as file1:
         for line in file1:
             chrom, rna_length = line.strip().split('\t')
             genes_to_add = [chrom] * (int(rna_length))
@@ -102,7 +102,6 @@ if __name__ == '__main__':
             for p in range(0, int(rna_length)):
                 number_list.append(p)
     file1.close()
-    init_file, trep_file = sys.argv[2], sys.argv[3]
     # handle fasta file
     # fasta_file_path = sys.argv[4]
     myfasta = []
@@ -121,8 +120,88 @@ if __name__ == '__main__':
     # also need to see how we generalize the code for the fasta file, maybe consider putting it
     # as part of the array? done in theory
 
-    myinit, my3p, mycov, mylength = covAndLen(init_file, trep_file)
-    Sa, Sb, Sc = calculateScores(number_list, mycov, mylength)
+    myinit, my3p, mycov, mylength = covAndLen(init_file_path, three_p_file_path)
+    Sa, Sb, Sc = calculateScores(number_list, mycov, mylength, window_size,W)
     createFile(gene_list_per_base_pair, Sa, Sb, Sc, myinit, my3p, mycov, number_list)
     # , fasta_as_list
-# , new_sno_df
+    # , new_sno_df
+
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # Create the main layout
+        layout = QVBoxLayout()
+
+        # Create the group box
+        group_box = QGroupBox("Group Box")
+        form_layout = QFormLayout()
+
+        # Create the QLineEdit and QComboBox widgets
+        combo_box = QComboBox()
+        combo_box.addItem("PRS")
+        combo_box.addItem("Total RNA")
+        labels_and_inputs = [("Sequencing Type:", combo_box), ("Window Size:", QLineEdit()),
+                             ("Genome File Path:", QLineEdit()),
+                             ("init File Path:", QLineEdit()), ("3p File Path:", QLineEdit()),
+                             ("Fasta File Path:", QLineEdit()), ("Output File Name:", QLineEdit())]
+
+        for i in range(len(labels_and_inputs)):
+            label, input_var = labels_and_inputs[i]
+
+            form_layout.addRow(label, input_var)
+
+        group_box.setLayout(form_layout)
+
+        # Add the group box to the main layout
+        layout.addWidget(group_box)
+
+        # Create the QDialogButtonBox
+        button_box = QDialogButtonBox()
+        button_box.setStandardButtons(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+
+        # Connect the Ok button's clicked signal to the on_ok_button_clicked function
+        button_box.accepted.connect(self.on_ok_button_clicked)
+
+        # Add the button box to the main layout
+        layout.addWidget(button_box)
+
+        self.setLayout(layout)
+
+    def on_ok_button_clicked(self):
+        # Retrieve the inputs from the QLineEdit widgets and the selected option from the QComboBox
+        inputs = [self.findChild(QComboBox).currentText()]
+
+        line_edits = self.findChildren(QLineEdit)
+        for line_edit in line_edits:
+            inputs.append(line_edit.text())  # Get the text from each QLineEdit
+
+        # Pass the inputs to another function for further processing
+        process_inputs(inputs)
+
+        # Close the dialog or perform other actions as needed
+        self.close()
+
+
+def process_inputs(inputs):
+    # Perform your logic on the inputs
+    sequencing_type = inputs[0]
+    window_size = int(inputs[1])
+    genome_file_path = inputs[2]
+    init_file_path = inputs[3]
+    three_p_file_path = inputs[4]
+    fasta_file_path = inputs[5]
+    output_file_name = inputs[6]
+
+    runScript(sequencing_type, window_size, genome_file_path, init_file_path, three_p_file_path, fasta_file_path,
+              output_file_name)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = MyWidget()
+    widget.show()
+    sys.exit(app.exec_())
